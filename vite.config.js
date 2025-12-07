69102caf4347c6ce00180523780ddd9f002605bf
Hochi <hochi@10-17-96-240.dynapool.wireless.nyu.edu>
2025-12-06 20:16:03 -0500

    Set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/what-is-aurora/',
   plugins: [
     vue(),
     vueDevTools(),
